7c8db9cc99b564c778ae30a59ea021b2b1ae3fc8
LawrenceM2867 <hcps-morganla2@henricostudents.org>
2025-04-28 21:40:34 +0000

    restored part 2

--- lessons/lessons.js
+++ lessons/lessons.js
@@ -1,7 +1,20 @@
-function index_setLesson(lessonNum) { //sets the lesson # to use
-    sessionStorage.setItem("current_lesson", lessonNum)
+var current_lessons = []
+
+function index_addLesson(lessonNum) { //sets the lesson # to use
+    current_lessons.push(lessonNum) // add a lesson to the current lessons
+    sessionStorage.setItem("current_lessons", JSON.stringify(current_lessons))
 }
 
 function lessons_onload() { //runs when the lessons page opens up
-    document.getElementById("title").innerHTML = "Lesson " + sessionStorage.getItem("current_lesson") //set the title to the lesson
+    current_lessons = JSON.parse(sessionStorage.getItem("current_lessons"))
+    var title = "Lesson "
+    for (num in current_lessons) {
+        if (title == "Lesson ") {
+            title += current_lessons[num]
+        } else {
+            title += ", " + current_lessons[num]
+        }
+    }
+
+    document.getElementById("title").innerHTML = title //set the title to the lesson
 }
