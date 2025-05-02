69066f386653cf57bd1d70412b262b62b456929a
LawrenceM2867 <hcps-morganla2@henricostudents.org>
2025-05-02 13:04:53 +0000

    fixed your css

--- lessons/lessons.js
+++ lessons/lessons.js
@@ -1,10 +1,28 @@
 var current_lessons = []
 
-function index_addLesson(lessonNum) { //sets the lesson # to use
+function index_onload() {
+    sessionStorage.setItem("current_lessons", JSON.stringify([]))
+    document.getElementById("gotoall").style.display = "none"
+}
+
+function index_addLesson(lessonNum, box) { //sets the lesson # to use
     if (!current_lessons.includes(lessonNum)) {
         current_lessons.push(lessonNum) // add a lesson to the current lessons
-        sessionStorage.setItem("current_lessons", JSON.stringify(current_lessons))
+    } else {
+        current_lessons.splice(current_lessons.indexOf(lessonNum), 1)
     }
+
+    sessionStorage.setItem("current_lessons", JSON.stringify(current_lessons))
+
+    if (current_lessons.length != 0) {
+        document.getElementById("gotoall").style.display = "block"
+    } else {
+        document.getElementById("gotoall").style.display = "none"
+    }
+}
+
+function index_setLesson(lessonNum) {
+    sessionStorage.setItem("current_lessons", JSON.stringify([lessonNum]))
 }
 
 function lessons_onload() { //runs when the lessons page opens up
